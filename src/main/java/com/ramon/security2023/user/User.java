7473e60c6417f38a2eb96040c6bf72483c656b7a
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-02-22 12:21:11 -0300

    Mapeamento da Classe User

--- src/main/java/com/ramon/security2023/user/User.java
+++ src/main/java/com/ramon/security2023/user/User.java
@@ -1,5 +1,10 @@
 package com.ramon.security2023.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,8 +14,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
+@Table(name="_user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
