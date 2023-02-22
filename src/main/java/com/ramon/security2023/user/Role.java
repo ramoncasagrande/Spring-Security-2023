05d83329b2eaa283f068800746f14f12ecbf6432
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-02-22 12:42:34 -0300

    Criado Enum Role

---
+++ src/main/java/com/ramon/security2023/user/Role.java
@@ -0,0 +1,8 @@
+package com.ramon.security2023.user;
+
+public enum Role {
+
+    USER,
+    ADMIN
+
+}
--- src/main/java/com/ramon/security2023/user/User.java
+++ src/main/java/com/ramon/security2023/user/User.java
@@ -6,6 +6,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -30,6 +32,9 @@ public class User implements UserDetails{
     private String lastName;
     private String email;
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
     
     
     @Override
