a78be77a8ef28d58979a278a120eee5bdf0073bc
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-02-22 11:32:06 -0300

    Commit inicial

---
+++ src/main/java/com/ramon/security2023/Security2023Application.java
@@ -0,0 +1,13 @@
+package com.ramon.security2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Security2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramon/security2023/Security2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramon.security2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Security2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
