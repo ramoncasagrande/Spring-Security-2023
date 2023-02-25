dddb96d2d178eab9426d9670b78e97bc8a4fe540
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-02-25 09:53:00 -0300

    Criado método extractAllClaims

--- src/main/java/com/ramon/security2023/config/JwtService.java
+++ src/main/java/com/ramon/security2023/config/JwtService.java
@@ -2,10 +2,23 @@ package com.ramon.security2023.config;
 
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
 @Service
 public class JwtService {
     
     public String extractUserEmail(String token){
         return null;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+            .parserBuilder()
+            .setSigningKey(getSignInKey())
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+
     }
 }
