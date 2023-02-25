30ec5d91ab5eb80061162cc921dc49886fd517c9
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-02-25 10:23:12 -0300

    Criado método getSignInKey

--- src/main/java/com/ramon/security2023/config/JwtService.java
+++ src/main/java/com/ramon/security2023/config/JwtService.java
@@ -1,12 +1,19 @@
 package com.ramon.security2023.config;
 
+import java.security.Key;
+
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
+
+    //chave gerada no https://allkeysgenerator.com/
+    private static final String SECRET_KEY = "4E635266556A586E3272357538782F413F442A472D4B6150645367566B597033";
     
     public String extractUserEmail(String token){
         return null;
@@ -19,6 +26,10 @@ public class JwtService {
             .build()
             .parseClaimsJws(token)
             .getBody();
+    }
 
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
