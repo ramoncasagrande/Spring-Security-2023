c1639e3af90b1b33b33a10f805b98e16cab2b0f7
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-03-01 12:57:36 -0300

    Criado authentication provider bean

--- src/main/java/com/ramon/security2023/config/ApplicationConfig.java
+++ src/main/java/com/ramon/security2023/config/ApplicationConfig.java
@@ -2,9 +2,12 @@ package com.ramon.security2023.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.ramon.security2023.user.UserRepository;
 
@@ -20,6 +23,19 @@ public class ApplicationConfig {
     public UserDetailsService userDetailsService(){
         return username -> repository.findByEmail(username)
             .orElseThrow(() -> new UsernameNotFoundException("User not Found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
     
 }
